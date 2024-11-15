43f33ae4095719e0ce0ab489851bc4b715295d66
Choi-Jeemin <cjmin2004@naver.com>
2024-11-15 11:11:32 +0900

    add init files

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Main {
+    void run(){
+        System.out.println("Hello, Would!");
+    }
+    
+    public static void main(String[] args) {
+        Main m = new Main();
+        m.run();
+    }
+}
