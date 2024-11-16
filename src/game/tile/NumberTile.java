be809baa439e08b2240cad7f01ae59177f995a65
byb0823 <byb0823@naver.com>
2024-11-16 16:13:39 +0900

    feat: Add parameter to constructor of NumberTile class

--- src/game/tile/NumberTile.java
+++ src/game/tile/NumberTile.java
@@ -4,7 +4,8 @@ public class NumberTile extends Tile {
 
     private int number;
 
-    public NumberTile(TileType tileType, TileColor tileColor) {
+    public NumberTile(TileType tileType, TileColor tileColor, int number) {
         super(tileType, tileColor);
+        this.number = number;
     }
 }
