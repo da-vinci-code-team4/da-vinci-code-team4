374d6aba1d12c87cce5f7f0bd3ceaaa0429d92d1
byb0823 <byb0823@naver.com>
2024-11-16 14:38:32 +0900

    feat: Add NumberTile class
    NumberTile extends Tile class.

---
+++ src/game/tile/NumberTile.java
@@ -0,0 +1,10 @@
+package game.tile;
+
+public class NumberTile extends Tile {
+
+    private int number;
+
+    public NumberTile(TileType tileType, TileColor tileColor) {
+        super(tileType, tileColor);
+    }
+}
