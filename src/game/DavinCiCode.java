77e390dd64100cefeef5e9e45e9f508cb8523889
byb0823 <byb0823@naver.com>
2024-11-16 16:22:57 +0900

    feat: Add constants
    Add constants NUMBER_OF_INIT_TILE

--- src/game/DavinCiCode.java
+++ src/game/DavinCiCode.java
@@ -7,6 +7,7 @@ public class DavinCiCode {
 
     public static final int MIN_TILE_NUMBER = 0;
     public static final int MAX_TILE_NUMBER = 11;
+    public static final int NUMBER_OF_INIT_TILE = 4; //게임 시작 시 각 플레이어에게 분배되는 타일의 수
 
     private final TileManager tileManager;
     private final Player firstPlayer;
