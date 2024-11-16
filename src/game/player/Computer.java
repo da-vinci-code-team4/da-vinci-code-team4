be45e076b852b8beb569874ac1b8b15822d606dc
EverJun2 <deverjun2@gmail.com>
2024-11-16 17:35:19 +0900

    player class 1. turnStart 부분 2. getTileFromDeque 부분 제외 완성

--- src/game/player/Computer.java
+++ src/game/player/Computer.java
@@ -2,10 +2,11 @@ package game.player;
 
 import game.tile.Tile;
 import game.tile.TileManager;
+import java.util.Random;
 
 public class Computer extends Player{
-    public Computer(TileManager tileManager, String name) {
-        super(tileManager, name);
+    public Computer(TileManager tileManager, String name, int rank, int score) {
+        super(tileManager, name, rank, score);
     }
 
     @Override
@@ -20,6 +21,12 @@ public class Computer extends Player{
 
     @Override
     public boolean chooseToKeepTurn() {
+        //50프로 확률로 선택
+        Random rand = new Random();
+        int val = rand.nextInt(0,2);
+        if(val == 0){
+            return true;
+        }
         return false;
     }
 }
