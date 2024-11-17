44d1fe96cc7ec4c0eda5bcbe75d8f208a52329b1
EverJun2 <deverjun2@gmail.com>
2024-11-17 21:36:10 +0900

    Revert "add main class"
    This reverts commit 5c7ff6235b29552e4fc7622405a947c16b9612e8.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,61 +1,12 @@
 package main;
 
-import game.DavinCiCode;
-import game.player.Player;
-import java.util.Scanner;
-
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-
-    FileFactory fileFactory = new FileFactory();
-
-    private Player user;
-    private Player computer;
-
+    void run(){
+        System.out.println("Hello, Would!");
+    }
+    
     public static void main(String[] args) {
-        Main program = new Main();
-        program.run();
-    }
-
-    void menu() {
-        //테스트용 메뉴로 스윙 작업 시 변경 예정
-        int inpMenu = scanner.nextInt();
-        while(true){
-            switch (inpMenu) {
-                case 1: //게임시작
-                    DavinCiCode game = new DavinCiCode(Player firstPlayer, Player secondPlayer);
-                    break;
-                case 2: //현황보기
-                    viewStatus();
-                    break;
-                case 3: //기록보기
-                    viewRecordList();
-                    break;
-                case 4: //랭크보기
-                    viewRanking();
-                    break;
-            }
-
-            if(inpMenu == 0) {
-                break;
-            }
-        }
-
-    }
-
-    private void viewStatus() {
-
-    }
-
-    private void viewRecordList() {
-
-    }
-
-    private void viewRanking() {
-
-    }
-
-    void run() {
-        menu();
+        Main m = new Main();
+        m.run();
     }
 }
