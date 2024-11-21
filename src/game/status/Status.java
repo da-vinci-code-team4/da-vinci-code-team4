e23933b72c731ad000b5793305ac0f37c41aedc2
byb0823 <byb0823@naver.com>
2024-11-21 20:00:59 +0900

    feat: Status 클래스 메서드 추가
    1. saveSelectedTile: 플레이어가 고른 상대방 타일을 저장하는 메서드.
    2. saveGuessedTile: 플레이어가 예상한 타일 정보를 저장하는 메서드.
    3. saveResult: 턴 결과를 저장하는 메서드, ture면 상대방 타일을 맞춘 것.
    4. saveOpenedTile: 턴에서 공개된 타일을 저장하는 메서드.

--- src/game/status/Status.java
+++ src/game/status/Status.java
@@ -5,14 +5,30 @@ import game.tile.Tile;
 public class Status {
 
     private Tile drawTile;
+    private Tile selectedTile;
 
     public void saveDrawTile(Tile tile) {
         drawTile = tile;
     }
 
+    public void saveSelectedTile(Tile tile) {
+        selectedTile = tile;
+    }
+    public void saveGuessedTile(Tile clone) {
+
+    }
+    public void saveResult(boolean b) {
+
+    }
+
+    public void saveOpenedTile(Tile tile) {
+
+    }
     int cntRemain;
     int cntOpponentCorrectTile;
+
     int cntMyCorrectTile;
+
     int cntOpponentRemainTile;
     int cntMyRemainTile;
 
@@ -23,47 +39,49 @@ public class Status {
         this.cntOpponentRemainTile = 0;
         this.cntMyRemainTile = 0;
     }
-
     //분배 해야할 남은 타일 수 업데이트
+
     public void updateCntRemain(int number) {
         for (int i = 0; i < number; i++) {
             this.cntRemain--;
         }
     }
-
     //상대가 맞춘 내 타일 개수 업데이트
+
     public void updateCntOpponentCorrectTile() {
         this.cntOpponentCorrectTile++;
     }
-
     //내가 맞춘 상대 타일 개수 업데이트
+
     public void updateCntMyCorrectTile() {
         this.cntMyCorrectTile++;
     }
-
     //상대 남은 타일 개수 업데이트 (+)
+
     public void updateCntOpponentRemainTile(int number) {
         this.cntOpponentRemainTile++;
     }
-
     //내 남은 타일 개수 업데이트 (+)
+
     public void updateCntMyRemainTile(int number) {
         this.cntMyRemainTile++;
     }
-
     //상대 남은 타일 개수 업데이트 (-)
+
     public void updateCntOpponentRemainTile() {
         this.cntOpponentRemainTile--;
     }
-
     //내 남은 타일 개수 업데이트 (-)
+
     public void updateCntMyRemainTile() {
         this.cntMyRemainTile--;
     }
 
     //모든 타일이 맞춰졌나요?
+
     public boolean isAllTileOpened() {
         return cntRemain == 0;
     }
+
 
 }
