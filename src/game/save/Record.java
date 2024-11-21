afd0900f2d0f941e4bba1b0218960051b1fa2d75
byb0823 <byb0823@naver.com>
2024-11-21 18:31:02 +0900

    feat: Add parameter

--- src/game/save/Record.java
+++ src/game/save/Record.java
@@ -15,23 +15,25 @@ public class Record {
     private final Player player;
 
     /**각 턴 상황을 저장*/
-    private  Status status;
+    private final Status status;
 
     /**
      * @param turn 몇번 째 턴인지 입력 받는다
      * @param player 어떤 플레이어의 턴인지 입력 받는다
      */
-    private Record(int turn, Player player) {
+    private Record(int turn, Player player, Status status) {
         this.turn = turn;
         this.player = player;
+        this.status = status;
     }
 
     /**
      * @param turn 몇번 째 턴인지 입력 받는다
      * @param player 어떤 플레이어의 턴인지 입력 받는다
+     * @param status 게임 턴 별 상황을 입력 받는다
      * @return 생성된 객체를 반환한다
      */
-    public static Record of(int turn, Player player) {
-        return new Record(turn, player);
+    public static Record of(int turn, Player player, Status status) {
+        return new Record(turn, player, status);
     }
 }
