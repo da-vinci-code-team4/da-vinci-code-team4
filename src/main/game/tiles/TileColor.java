29496d4aa34c8f61abccf0564e1e6426d470736f
Jeemin <cjmin2004@naver.com>
2024-11-22 11:51:21 +0900

    gradle환경 구축

--- src/main/game/tiles/TileColor.java
+++
@@ -1,9 +0,0 @@
-package main.game.tiles;
-
-public enum TileColor {
-    BLACK("흑"),
-    WHITE("백");
-
-    TileColor(String color) {
-    }
-}
--- src/main/game/tiles/TileType.java
+++
@@ -1,9 +0,0 @@
-package main.game.tiles;
-
-public enum TileType {
-    JOKER("joker"),
-    NUMBER("number");
-
-    TileType(String type) {
-    }
-}
