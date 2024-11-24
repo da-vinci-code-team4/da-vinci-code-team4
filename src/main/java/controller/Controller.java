7c26fa09ab06891af8dd3e027ca67c4e33b7dde6
EverJun2 <deverjun2@gmail.com>
2024-11-24 16:55:25 +0900

    add getDeckSize() method
    1. 컨트롤러에서 각 타일 덱의 길이를 받아오는 메소드 추가

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,13 +6,31 @@ import game.player.User;
 import game.tile.TileManager;
 
 public class Controller {
+    private static TileManager tileManager;
+    private static Player firstPlayer;
+    private static Player secondPlayer;
+
     public static void startGame(){
         System.out.println("게임시작");
-        Player firstPlayer = new User("test1",100,100);
-        Player secondPlayer = new User("test2",100,23991);
-        TileManager tileManager = new TileManager(firstPlayer, secondPlayer);
+
+        firstPlayer = new User("test1",100,100);
+        secondPlayer = new User("test2",100,23991);
+        tileManager = new TileManager(firstPlayer, secondPlayer);
+
         GameManager game = new GameManager(tileManager, firstPlayer, secondPlayer);
+
         game.startGame();
     }
 
+    public static int getFirstPlayerDeckSize() {
+        return firstPlayer.getDeckSize();
+    }
+
+    public static int getSecondPlayerDeckSize() {
+        return secondPlayer.getDeckSize();
+    }
+
+    public static int getTileManagerSize() {
+        return tileManager.getDeckSize();
+    }
 }
